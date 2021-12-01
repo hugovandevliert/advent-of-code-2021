7581be356c1fdd5d598177b3601749ffd8d90961
hugovandevliert <hugo.vandevliert@floryn.com>
2021-12-01 22:38:05 +0100

    Day 1: second puzzle

--- day_1/puzzle_2.rb
+++ day_1/puzzle_2.rb
@@ -2002,11 +2002,16 @@ input = [
 ]
 
 count = 0
-prev_num = input.first
+prev_sum = input.first(3).sum
 
-input.each do |num|
-  count += 1 if num > prev_num
-  prev_num = num
+input.each_with_index do |n, index|
+  n2 = input[index + 1]
+  n3 = input[index + 2]
+  next unless n2 && n3
+
+  sum = n + n2 + n3
+  count += 1 if sum > prev_sum
+  prev_sum = sum
 end
 
 puts count
