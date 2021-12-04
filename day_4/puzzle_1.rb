d93510345854a8c0f5744e8b0c7d7106c4565357
hugovandevliert <hugo.vandevliert@floryn.com>
2021-12-04 19:02:05 +0100

    Day 4: first puzzle

---
+++ day_4/puzzle_1.rb
@@ -0,0 +1,40 @@
+input = File.read('input/day_4').split("\n")
+
+numbers = input.first.split(',').map(&:to_i)
+bingo_boards = input.drop(1).each_slice(6).map { |b| b.drop(1) }
+
+def board_wins?(bingo_board, numbers)
+  rows = bingo_board.map { |r| r.split.map(&:to_i) }
+  cols = rows.transpose
+
+  rows.each do |row|
+    return true if row.reject { |n| numbers.include? n }.empty?
+  end
+
+  cols.each do |col|
+    return true if col.reject { |n| numbers.include? n }.empty?
+  end
+
+  false
+end
+
+def calculate_score(bingo_board, numbers)
+  rows = bingo_board.map { |r| r.split.map(&:to_i) }
+  unmarked_numbers = rows.flatten.reject { |n| numbers.include? n }
+
+  unmarked_numbers.sum * numbers.last
+end
+
+def winning_board(bingo_boards, numbers)
+  numbers.count.times do |i|
+    numbers_so_far = numbers.first(i + 1)
+
+    bingo_boards.each do |board|
+      next unless board_wins?(board, numbers_so_far)
+
+      return calculate_score(board, numbers_so_far)
+    end
+  end
+end
+
+puts winning_board(bingo_boards, numbers)
