98a5bc104296858da7200e02cacf8919c1528b94
Hugo van de Vliert <hvandevliert@icloud.com>
2022-01-16 23:00:44 +0100

    Day 12: first puzzle

---
+++ day_12/puzzle_1.rb
@@ -0,0 +1,28 @@
+input = File
+  .read('input/day_12')
+  .split("\n")
+  .map { |l| l.split('-') }
+
+input += input.map { |a, b| [b, a] }
+paths = input.uniq.reject { |_, b| b == 'start' }
+
+def walk(route, paths, routes = [])
+  if route.last == 'end'
+    routes << route
+    return routes
+  end
+
+  options = paths
+    .select { |a, _| a == route.last }
+    .reject { |_, b| b.downcase == b && route.include?(b) }
+  return routes if options.empty?
+
+  options.each do |_, b|
+    walk(route + [b], paths, routes)
+  end
+
+  routes
+end
+
+routes = walk(['start'], paths)
+puts routes.count
